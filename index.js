964b837cd64fe73446346b400c017bf74827f0d3
bugnaw101 <radekmienkowski@gmail.com>
2025-11-25 16:08:36 +0100

    search by url

--- index.js
+++ index.js
@@ -4,21 +4,25 @@ const input = require("input");
 askStuff();
 
 async function askStuff() {
-    const name = await input.text('What is your search term?',{default:"trousers"});
+    const directUrl = await input.text('Paste full Vinted URL (leave empty to use manual search options):');
+    
+    let name, price, sex, sizes, pages;
 
-    const pages = await input.text("How many pages to scan?",{default:10});
+    if (!directUrl) {
+        pages = await input.text("How many pages to scan?",{default:10});
+        name = await input.text('What is your search term?',{default:"trousers"});
+        price = await input.text("Max price? (press enter if you don't want to search by price)");
+        sex = await input.select(`Male or Female?`, ['Male', 'Female']);
+        sizes = await input.checkboxes(`Select Size (space to select)`, (sex =="Male") ? [
+            'XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL','4XL','5XL','6XL','7XL','8XL'
+          ] : [
+            'XXXS', 'XXS', 'XS', 'S', 'M', 'L', 'XL','4XL','5XL','6XL','7XL','8XL',"9XL"
+          ]);
+    } else {
+        pages = await input.text("How many pages to scan?",{default:10});
+    }
 
-    const price = await input.text("Max price? (press enter if you don't want to search by price)");
-    const sex = await input.select(`Male or Female?`, [
-        'Male', 'Female']);
-    const sizes = await input.checkboxes(`Select Size (space to select)`, (sex =="Male") ? [
-        'XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL','4XL','5XL','6XL','7XL','8XL'
-      ] : [
-        'XXXS', 'XXS', 'XS', 'S', 'M', 'L', 'XL','4XL','5XL','6XL','7XL','8XL',"9XL"
-      ]);
-
-
-    if(name.length!=0&&isNumber(pages)) {
+    if( (directUrl || name.length!=0) && isNumber(pages)) {
         const browser = await puppeteer.launch({ 
             headless: false ,
             defaultViewport: null,
@@ -26,48 +30,48 @@ async function askStuff() {
 
         const page = await browser.newPage();
 
-        await page.goto("https://www.vinted.pl/catalog?search_text="+name);
+        if (directUrl) {
+            await page.goto(directUrl);
+        } else {
+            await page.goto("https://www.vinted.pl/catalog?search_text="+name);
+        }
 
         await delay(2000);
 
         if(await page.$("#onetrust-accept-btn-handler") !==null) {
             await page.click("#onetrust-accept-btn-handler");
-
             await delay(2000);
         }
 
+        if (!directUrl) {
+            try {
+                await page.click("button[data-testid=catalog--size-filter--trigger]");
 
+                if(sex=="Male") {
+                    await page.click("div[data-testid=dropdown-content] ul li:nth-child(2)");
+                } else {
+                    await page.click("div[data-testid=dropdown-content] ul li:first");
+                }
 
-        //size selection
-        await page.click("button[data-testid=catalog--size-filter--trigger]");
+                await delay(1000);
 
-        if(sex=="Male") {
-            await page.click("div[data-testid=dropdown-content] ul li:nth-child(2)");
-        } else {
-            await page.click("div[data-testid=dropdown-content] ul li:first");
+                for(size in sizes) {
+                    const el =(await page.$$("xpath/.//div[@data-testid='dropdown-content']//ul//span[text()='"+sizes[size]+"']"))[0];
+                    if (el) el.click();
+                }
+                
+                await delay(2000);
+
+                if(price!=""&&isNumber(price)) {
+                    await page.click("button[data-testid=catalog--price-filter--trigger]");
+                    await page.type('div[data-testid=dropdown-content] label[for=price_to] input', price, { delay: 10 });
+                    await page.click("body");
+                }
+                await delay(5000);
+            } catch (e) {
+                console.log("Error applying filters automatically: ", e);
+            }
         }
-
-        
-        await delay(1000);
-
-        for(size in sizes) {
-            const el =(await page.$$("xpath/.//div[@data-testid='dropdown-content']//ul//span[text()='"+sizes[size]+"']"))[0];
-            el.click();
-        }
-        
-        await delay(2000);
-
-        //setting max price
-        if(price!=""&&isNumber(price)) {
-            await page.click("button[data-testid=catalog--price-filter--trigger]");
-
-            await page.type('div[data-testid=dropdown-content] label[for=price_to] input', price, { delay: 10 });
-            await page.click("body");
-        }
-
-
-
-        await delay(5000);
 
         var itemsList = [];
         itemsList = await collectData(page);
@@ -86,9 +90,8 @@ async function askStuff() {
             } else {
                 break;
             }
-
-            
         }
+
         itemsList = [...itemsList.entries()].sort((a, b) => b[1]["likes"] - a[1]["likes"]);
 
         await page.evaluate((itemsList) => {
@@ -97,12 +100,13 @@ async function askStuff() {
 
             for(key in itemsList) {
                 console.log(itemsList[key]);
-                var li = jQuery("<li></li>");
-                jQuery(li).append(a);
-
+                
                 var a = jQuery("<a></a>");
                 a.attr("href",itemsList[key][1]["href"]);
                 a.attr("target","_blank");
+
+                var li = jQuery("<li></li>");
+                jQuery(li).append(a);
 
                 var img = jQuery("<img/>");
                 img.attr("src",itemsList[key][1]["src"]);
@@ -122,8 +126,6 @@ async function askStuff() {
 
             jQuery("body").append(list);
         },itemsList);
-
-
     }
 }
   
@@ -132,18 +134,18 @@ function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
 
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
-  } 
+} 
   
 
 async function collectData(page) {
-
     return await page.evaluate(async () => {
-        var jq = document.createElement('script');
+        if (typeof jQuery == 'undefined') {
+            var jq = document.createElement('script');
             jq.src = "https://code.jquery.com/jquery-3.7.1.min.js";
             document.getElementsByTagName('head')[0].appendChild(jq);
             await new Promise(r => setTimeout(r, 3000));
-            // ... give time for script to load, then type (or see below for non wait option)
             jQuery.noConflict();
+        }
 
         var map = [];
         jQuery(".feed-grid__item").each(function(i) {
